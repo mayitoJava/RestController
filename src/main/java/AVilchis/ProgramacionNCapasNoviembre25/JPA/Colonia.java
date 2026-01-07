bb04be378e6d352ef4f3c7f2569eb7006d6257ae
mariovilchis <mariovilchis01@hotmail.com>
2026-01-07 13:25:37 -0600

    Cliente/FormUpdate

--- src/main/java/AVilchis/ProgramacionNCapasNoviembre25/JPA/Colonia.java
+++ src/main/java/AVilchis/ProgramacionNCapasNoviembre25/JPA/Colonia.java
@@ -1,6 +1,5 @@
 package AVilchis.ProgramacionNCapasNoviembre25.JPA;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
